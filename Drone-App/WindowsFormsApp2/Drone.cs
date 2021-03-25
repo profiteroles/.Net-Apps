8b1b706817142c55262d6d510767833e0a184e24
profiteroles <erollooper@gmail.com>
2021-03-25 19:14:36 +0800

    Drone App

---
+++ Drone-App/WindowsFormsApp2/Drone.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    class Drone
+    {
+        private string engine, range, accessory, price; // private variables
+
+        public string gsEngine // getter and setter for engine variable
+        {
+            get { return engine; }
+            set { engine = value; }
+        }
+        public string gsRange// getter and setter for range variable
+        {
+            get { return range; }
+            set { range = value; }
+        }
+        public string gsAccessory// getter and setter for accessory variable
+        {
+            get { return accessory; }
+            set { accessory = value; }
+        }
+        public string gsPrice// getter and setter for price variable
+        {
+            get { return price; }
+            set { price = value; }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # All-My-.Net-Application
 To make easy for myself and for my clients I collecting all my .NET app under one roof.
+
+Note to myself "I haven't got around to rerun the applications update and take a screenshot"

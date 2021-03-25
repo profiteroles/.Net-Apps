a9c4a9284dc83c15de034e83b4db605a6c120a38
profiteroles <erollooper@gmail.com>
2021-03-25 19:47:31 +0800

    Game App

---
+++ GameApp/GameApp/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace GameApp
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.textBoxPlatform = new System.Windows.Forms.TextBox();
+            this.textBoxGenre = new System.Windows.Forms.TextBox();
+            this.textBoxPublisher = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.statusMsg = new System.Windows.Forms.StatusStrip();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.clearListBoxButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(146, 83);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Game Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 146);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Platform";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(150, 210);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Genre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(150, 271);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Publisher";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(54, 331);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "My Games";
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 25;
+            this.listBox.Location = new System.Drawing.Point(60, 362);
+            this.listBox.Margin = new System.Windows.Forms.Padding(6);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(952, 304);
+            this.listBox.TabIndex = 5;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(274, 69);
+            this.textBoxTitle.Margin = new System.Windows.Forms.Padding(6);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(196, 31);
+            this.textBoxTitle.TabIndex = 6;
+            // 
+            // textBoxPlatform
+            // 
+            this.textBoxPlatform.Location = new System.Drawing.Point(274, 133);
+            this.textBoxPlatform.Margin = new System.Windows.Forms.Padding(6);
+            this.textBoxPlatform.Name = "textBoxPlatform";
+            this.textBoxPlatform.Size = new System.Drawing.Size(196, 31);
+            this.textBoxPlatform.TabIndex = 7;
+            // 
+            // textBoxGenre
+            // 
+            this.textBoxGenre.Location = new System.Drawing.Point(274, 196);
+            this.textBoxGenre.Margin = new System.Windows.Forms.Padding(6);
+            this.textBoxGenre.Name = "textBoxGenre";
+            this.textBoxGenre.Size = new System.Drawing.Size(196, 31);
+            this.textBoxGenre.TabIndex = 8;
+            // 
+            // textBoxPublisher
+            // 
+            this.textBoxPublisher.Location = new System.Drawing.Point(274, 258);
+            this.textBoxPublisher.Margin = new System.Windows.Forms.Padding(6);
+            this.textBoxPublisher.Name = "textBoxPublisher";
+            this.textBoxPublisher.Size = new System.Drawing.Size(196, 31);
+            this.textBoxPublisher.TabIndex = 9;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(532, 69);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(150, 44);
+            this.buttonAdd.TabIndex = 10;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(532, 129);
+            this.buttonSearch.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(150, 44);
+            this.buttonSearch.TabIndex = 11;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(532, 190);
+            this.buttonClear.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(150, 44);
+            this.buttonClear.TabIndex = 12;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(866, 706);
+            this.buttonLoad.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(150, 44);
+            this.buttonLoad.TabIndex = 13;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // statusMsg
+            // 
+            this.statusMsg.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusMsg.Location = new System.Drawing.Point(0, 813);
+            this.statusMsg.Name = "statusMsg";
+            this.statusMsg.Padding = new System.Windows.Forms.Padding(2, 0, 28, 0);
+            this.statusMsg.Size = new System.Drawing.Size(1070, 22);
+            this.statusMsg.TabIndex = 14;
+            this.statusMsg.Text = "statusMsg";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(532, 258);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(150, 44);
+            this.buttonSave.TabIndex = 15;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // clearListBoxButton
+            // 
+            this.clearListBoxButton.Location = new System.Drawing.Point(60, 706);
+            this.clearListBoxButton.Margin = new System.Windows.Forms.Padding(6);
+            this.clearListBoxButton.Name = "clearListBoxButton";
+            this.clearListBoxButton.Size = new System.Drawing.Size(150, 44);
+            this.clearListBoxButton.TabIndex = 16;
+            this.clearListBoxButton.Text = "Clear List Box";
+            this.clearListBoxButton.UseVisualStyleBackColor = true;
+            this.clearListBoxButton.Click += new System.EventHandler(this.clearListBoxButton_Click);
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1070, 835);
+            this.Controls.Add(this.clearListBoxButton);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.statusMsg);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxPublisher);
+            this.Controls.Add(this.textBoxGenre);
+            this.Controls.Add(this.textBoxPlatform);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "GameForm";
+            this.Text = "Game App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.TextBox textBoxPlatform;
+        private System.Windows.Forms.TextBox textBoxGenre;
+        private System.Windows.Forms.TextBox textBoxPublisher;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.StatusStrip statusMsg;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button clearListBoxButton;
+    }
+}
+
---
+++ GameApp/GameApp/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GameApp
+{
+    public partial class GameForm : Form
+    {//declared variables
+        const int maxRow = 4, maxCol = 10;
+        string[,] data = new string[maxRow, maxCol];        
+        static string saveGameFile = "save.txt";
+        private int emptyPtr = 0;        
+
+        public GameForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear(); // Clears the List Box to load a File
+            BinaryReader br;
+            try {
+                if (File.Exists(saveGameFile)) //Tries to open the mybikes.dat file
+                {
+                    try
+                    {//create new binary Reader indicating the save file 
+                        br = new BinaryReader(File.Open(saveGameFile, FileMode.Open));
+
+                        emptyPtr = 0;
+
+                        for (int c = 0; c < maxCol; c++)
+                        { // displays saved files in the list box
+                            for (int r = 0; r < maxRow; r++)
+                            {
+                                data[r, c] = br.ReadString();
+                                statusMsg.Items.Clear();
+                                statusMsg.Items.Add("File Has Loaded");
+                            }
+                            emptyPtr++;
+                            Display();
+                            
+                        }
+
+                    }
+                    catch (EndOfStreamException ex)
+                    { // cathes error and informs the user
+                        statusMsg.Items.Clear();
+                        statusMsg.Items.Add("Pass the Error: End Of Stream Exception & File Has Loaded");
+                        return;                        
+                    }
+                }
+            }
+            catch (IOException ed)
+            {
+                statusMsg.Items.Clear();
+                statusMsg.Items.Add("Error: IOExcption and Can't Load the File");
+                return;
+            }
+        }
+
+        public void BubbleSort()// sort using the bubble sort (the swap is builtin)
+        {
+            for (int r = 0; r < emptyPtr; r++)
+            {
+                for (int c = 0; c < emptyPtr - 1; c++)
+                {
+                    if (data[0, c].CompareTo(data[0, c + 1]) > 0)
+                    {
+                        Swap(c);
+                    }
+                }
+            }
+        }
+
+        public void Swap(int i)
+        {
+            for (int r = 0; r < maxRow; r++)
+            {
+                string temp = data[r, i];
+                data[r, i] = data[r, i + 1];
+                data[r, i + 1] = temp;
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxTitle.Clear();
+            textBoxPlatform.Clear();
+            textBoxGenre.Clear();
+            textBoxPublisher.Clear();
+            statusMsg.Items.Clear();
+            statusMsg.Items.Add("Text Boxes are Cleared");
+        }
+
+        public void Display()
+        {
+            listBox.Items.Clear();
+            for (int c = 0; c < emptyPtr; c++)
+            {
+                listBox.Items.Add($"{data[0, c] + "\t" + data[1, c] + "\t" + data[2, c] + "\t" + data[3, c]}");
+            }
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+
+            string keyWord = textBoxTitle.Text;
+            BSearch(data, keyWord);
+        }
+
+        public void BSearch(string[,] array, string keyWord)
+        {// Declared variables for binary search
+            int mid = 0;
+            int low = 0;
+            int high = emptyPtr - 1;
+            Boolean found = false;
+            string target = textBoxTitle.Text;
+
+            if (!(string.IsNullOrEmpty(textBoxTitle.Text))) // if the Game Title text box is not empty
+            {
+                try
+                {
+                    while (found != true && low <= high) // loops it until it founds
+                    {
+                        
+                        for (int i = 0; i < maxRow; i++) // goes through the maxtrix
+                        {
+                            if (data[i, mid] == target) // if finds it
+                            {
+                                found = true;
+                                listBox.SetSelected(mid, found);// selects the row 
+                                statusMsg.Items.Clear();
+                                statusMsg.Items.Add("Found it " + data[i, mid]);                                
+                            }
+                            else // if not then passes the message to user
+                            {                                
+                                mid++;
+                                low++;
+                            }
+
+                        }
+                    }
+                }
+                
+                catch (Exception c) // to prevent any errors
+                {
+                    statusMsg.Items.Clear();
+                    statusMsg.Items.Add("Error");
+                }
+            }
+            else // if the game title box is empty warns the user
+            {
+                MessageBox.Show("Insert the Game Title You'd like to Search");
+            }
+        }        
+        
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            try
+            { //Binary witer to save the values inside the arrays
+                using (BinaryWriter bw = new BinaryWriter(new FileStream(saveGameFile, FileMode.Create)))
+                {
+                    for (int c = 0; c < emptyPtr; c++)
+                    {
+                        for (int r = 0; r < maxRow; r++) {
+                            bw.Write(data[r, c]);
+                        }
+                    }
+                    //Shows Message that data has been saved
+                    MessageBox.Show("Your File Has Saved", "Notification Message");
+                    statusMsg.Items.Clear();
+                    statusMsg.Items.Add("Your file is in save hands");
+                    bw.Close();
+                }
+            }
+            catch // catches an error if data is missing on the list box
+            {
+                MessageBox.Show("Enter the data to save it", "Error Message");
+                statusMsg.Items.Clear();
+                statusMsg.Items.Add("Data Need to be on the List Box to Save it!!!");
+                return;
+            }
+        }
+
+
+        private void clearListBoxButton_Click(object sender, EventArgs e)
+        {// clears everything
+            textBoxTitle.Clear();
+            textBoxPlatform.Clear();
+            textBoxGenre.Clear();
+            textBoxPublisher.Clear();
+            listBox.Items.Clear();
+            Array.Clear(data, 0, data.Length);
+            emptyPtr = 0;
+            statusMsg.Items.Clear();
+            statusMsg.Items.Add("Text Boxes are Cleared");
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox.SelectedItem != null) // when item selected it appears on the text boxes
+            {                
+                textBoxTitle.Text = data[0,listBox.SelectedIndex];
+                textBoxPlatform.Text = data[1,listBox.SelectedIndex];
+                textBoxGenre.Text = data[2, listBox.SelectedIndex];
+                textBoxPublisher.Text = data[3, listBox.SelectedIndex];
+                statusMsg.Items.Clear();
+                statusMsg.Items.Add("Selected Item Displays on Text Boxes");
+            }
+        }
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (emptyPtr >= maxCol) // if the list is on the capacity informs the user
+            {
+                MessageBox.Show("List is Full", "Important Information");
+            }
+            else// if there is a room adds to the list box
+            {
+                try
+                {//if none of those textboxes are not empty adds to the list box
+                    if (!(string.IsNullOrEmpty(textBoxTitle.Text)) &&
+                        !(string.IsNullOrEmpty(textBoxPlatform.Text)) &&
+                        !(string.IsNullOrEmpty(textBoxGenre.Text)) &&
+                        !(string.IsNullOrEmpty(textBoxPublisher.Text)))
+                    {                                                    
+                        data[0, emptyPtr] = textBoxTitle.Text;
+                        data[1, emptyPtr] = textBoxPlatform.Text;
+                        data[2, emptyPtr] = textBoxGenre.Text;
+                        data[3, emptyPtr] = textBoxPublisher.Text;
+                        listBox.Items.Clear();
+                        emptyPtr++;
+                        BubbleSort();
+                        Display();
+                        statusMsg.Items.Clear();
+                        statusMsg.Items.Add("Data Added to the List Box");
+                    }
+                    else// otherwise passes message
+                    {
+                        statusMsg.Items.Clear();
+                        statusMsg.Items.Add("Please Enter the Data");
+                    }
+                    }                
+
+                catch (FormatException ex) // catches errors 
+                {
+                    statusMsg.Items.Clear();
+                    statusMsg.Items.Add("Trying to pass an Error");
+                }
+            }
+        }
+    }
+}

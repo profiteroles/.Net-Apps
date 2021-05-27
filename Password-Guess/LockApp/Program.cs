d5a22ea2fc8b170be182707bec7c0d365fe96cb7
Erol <64768508+profiteroles@users.noreply.github.com>
2021-05-27 10:58:05 +0800

    Password Guess

---
+++ Password-Guess/LockApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int newCode = random.Next(1, 999);
+            int doorCode;
+            int counter = 0;
+            Boolean locked = true;
+            Console.WriteLine(newCode);
+            try
+            {
+                do
+                {
+                    Console.WriteLine("Please Enter 3 digit to Open Door Code!");
+                    doorCode = int.Parse(Console.ReadLine());
+                    counter++;
+
+                    if (doorCode == newCode)
+                    {
+                        Console.WriteLine("Correct Password - Door is Opened!");
+                        locked = false;
+                        Console.BackgroundColor = ConsoleColor.Green;
+                    }
+
+                    else if (newCode < doorCode)
+                    {
+                        Console.WriteLine("Number is too HIGh");
+                    }
+                    else if (newCode > doorCode)
+                    {
+                        Console.WriteLine("Number is too LOW");
+                    }
+                }
+                while (counter < 5);
+                while (!locked) ;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Please Enter Number not letter & try again");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Password-Guess/Readme.md
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<h2>Guessing Password App</h2>
+
+<div>
+This is part of the learning curve where I had to implement n get police involce incase password guessed incorrectly so many times.<br>
+<br>
+<br>
+</div>
+<div class="row">
+    <img src="shot/1.png" alt="Screenshot">
+    <img src="shot/2.png" alt="Screenshot">
+    <img src="shot/3.png" alt="Screenshot">
+</div>
+</body>
+</html>

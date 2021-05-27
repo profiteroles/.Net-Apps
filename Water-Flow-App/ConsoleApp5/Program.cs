347b5d7f27a3f7902f9b1737ac5ffbec20bcdd2a
Erol <64768508+profiteroles@users.noreply.github.com>
2021-05-27 10:45:38 +0800

    Water Flow

---
+++ Water-Flow-App/ConsoleApp5/Program.cs
@@ -0,0 +1,57 @@
+﻿            using System;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+
+namespace HydroelectricDam
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int flowRate;// declaring value
+                int minFlow = 70; // mininum Flowrate
+                int maxFLow = 150; // Maxisimum Flowrate
+
+                Console.WriteLine("What is the Flow Rate?"); 
+                if (int.TryParse(Console.ReadLine(), out flowRate)) // integer tryparses out as flowrate
+                {
+                    if (flowRate < minFlow) //if value less than 70 give a warning of "LOW"
+                    {
+                        Console.WriteLine("***Warning!! :Flow Rate Low");
+                        Console.WriteLine("Opening intake");
+
+                    }
+
+                    else if (flowRate > maxFLow)//if value bigger than 150 give a warning of "HIGH"
+                    {
+                        Console.WriteLine("***Warning!! : Flow Rate too High");
+                        Console.WriteLine("Closing intake");
+
+                    }
+                    else // otherwise keep things stady
+                    {
+                        Console.WriteLine("Flow Rate OK");
+                        Console.WriteLine("Normal flow");
+                    }
+
+                }
+                else // making sure that gives a correct value which an interger not string or double
+            {
+                Console.WriteLine("Enter Correct Value");
+            }
+
+                /*
+                 A'NIL EROL
+                 20.06.2020
+                 Hydroelectric Dam
+                 It show the flow rate of the Hydroelectric Dam. As you put level of the flow rate it show the status of the flow rate and has function to close and open the intake.      
+                 */
+            }
+
+        }
+    }
+
+    
+
---
+++ Water-Flow-App/Readme.md
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<h2>Water Flow Reader App</h2>
+
+<div>
+This program is such a simple console app where receives a data from user and sends and alert.<br>
+It could be good one or a dangerous one it all depends on the input.<br>
+<br>
+</div>
+<div class="row">
+    <img src="shot/1.png" alt="Screenshot">
+    <img src="shot/2.png" alt="Screenshot">
+    <img src="shot/3.png" alt="Screenshot">
+</div>
+</body>
+</html>
